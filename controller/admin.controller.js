f552e34da3e27828d8e9807101ec5939abdcfd38
shailendra153 <kushwahshailendra12345@gmail.com>
2022-02-22 14:05:47 +0530

    login sucess

--- controller/admin.controller.js
+++ controller/admin.controller.js
@@ -1,8 +1,21 @@
-
-
 const Admin = require('../model/admin.model');
-exports.adminLoginPage = (request,response,next)=>{
-    response.render("admin/admin_login.ejs",{
+exports.adminLoginPage = (request, response, next) => {
+    response.render("admin/admin_login.ejs", {
         title: "Login"
+    });
+};
+exports.adminLoginPost = (request, response, next) => {
+    const email = request.body.email;
+    const password = request.body.password;
+    let admin = new Admin(email, password);
+    admin.checkAdmin().then(results => {
+        if (results.length > 0) {
+            request.session.current_user = email;
+            response.redirect("/admin/dashboard");
+            console.log("Login Success")
+        } else
+            console.log("Login Failed...");
+    }).catch(err => {
+        console.log(err);
     });
 };
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,11 +1,9 @@
-const express  = require('express');
+const express = require('express');
 const indexController = require('../controller/admin.controller');
+const adminController = require('../controller/admin.controller')
 const router = express.Router();
 
-router.get("/",indexController.adminLoginPage);
+router.get("/", indexController.adminLoginPage);
 
-//  router.post("/login",adminController.adminLoginPost);
+router.post("/login", adminController.adminLoginPost);
 module.exports = router;
-
-
-
