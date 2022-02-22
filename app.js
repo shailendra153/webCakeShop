b95dd47c249ee3b778a80746d13d2a3c84937793
Durgesh <durgeshrathore060@gmail.com>
2022-02-22 11:43:57 +0530

    demo

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 const express = require('express');
-
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const path = require('path');
 const adminRouter = require("./routes/adminRoutes");
+const homeRouter = require("./routes/homeRoutes");
 
 const app = express();
+const fileupload = require('express-fileupload');
+
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(session({
+    secret: 'dfkjfrrereprxvncvncvnrorererp'
+}));
+app.use(fileupload());
 
-app.use(adminRouter);
+
+app.use("/admin",adminRouter);
+app.use(homeRouter);
 
 
 
 app.listen(3000);
+
+
+
+
